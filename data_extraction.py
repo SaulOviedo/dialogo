e1c66342b6b2c50bce53665fb3e40e91cc71715d
Saúl Oviedo <sauloviedoleon@gmail.com>
2021-10-15 18:34:24 -0400

    update

--- data_extraction.py
+++ data_extraction.py
@@ -15,12 +15,21 @@ def translate(data):
 
 
 # Comenzamos con el primer nodo
-maduro_id = 1252764865
-res = twitter.get_user(maduro_id)
+starting_nodes = [
+    1252764865, # Nicolas Maduro
+    128262354, # Diosdado Cabello
+    1644228493, # Delcy Rodriguez
+    109601997, # Jorge Rodríguez
+    39176902, # Juan Guaido
+    14119371, # Julio Borges
+    42434332, # Leopoldo Lopez
+    47491330, # Henrique Capriles
+]
+
+res = twitter.get_user(starting_nodes[4])
 df = pd.DataFrame.from_dict([ translate(res['data']) ])
 
 # Comenzamos a buscar
-
 user_id = df.iloc[0]['id']
 res = twitter.get_user_following(user_id)
 
@@ -31,6 +40,6 @@ for r in res['data']:
     ids.append( str(r['id']) )
 
 df.iloc[0, -1] = ','.join(ids)
-df = pd.concat( (df,  pd.DataFrame.from_dict(data)) ).reset_index()
+df = pd.concat( (df,  pd.DataFrame.from_dict(data)) ).set_index('id')
 
-print(df.head())
+print(df.head(50))
